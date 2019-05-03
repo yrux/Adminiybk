b29af23d8d919379d773128d33387a9e622e5884
yruxwork <yruxwork@gmail.com>
2019-05-03 06:54:54 +0500

    Update PanelUpdateController.php
    panel update from github directly

--- app/Http/Controllers/Adminiy/DNE/PanelUpdateController.php
+++ app/Http/Controllers/Adminiy/DNE/PanelUpdateController.php
@@ -34,19 +34,23 @@ class PanelUpdateController extends IndexController
         return $this->getGitV();
     }
     public static function getGitV(){
-        $endpoint = "https://raw.githubusercontent.com/yrux/Adminiy/master/public/admin/core-files.json";
+        //https://api.github.com/repos/yrux/Adminiy/contents
+        //https://api.github.com/repos/yrux/Adminiy/contents/public/admin/core-files.json?ref=master
+        //curl https://api.github.com/repos/yrux/Adminiy/contents/public/admin/core-files.json
+        $endpoint = "https://api.github.com/repos/yrux/Adminiy/contents/public/admin/core-files.json?ref=master";
         $client = new \GuzzleHttp\Client();
         $response = $client->request('GET',$endpoint);
         $statusCode = $response->getStatusCode();
-        $content = $response->getBody();
-        return $content;
+        $content = json_decode($response->getBody());
+        $data = base64_decode($content->content);
+        return $data;
     }
     public static function getGitFile($file){
-        $endpoint = "https://raw.githubusercontent.com/yrux/Adminiy/master/".$file;
+        $endpoint = "https://api.github.com/repos/yrux/Adminiy/contents/".$file;
         $client = new \GuzzleHttp\Client();
         $response = $client->request('GET',$endpoint);
         $statusCode = $response->getStatusCode();
-        $content = $response->getBody();
+        $content = base64_decode(json_decode($response->getBody())->content);
         return $content;
     }
 }
